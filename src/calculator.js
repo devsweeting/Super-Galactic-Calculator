2fbef4da287cb155bcc31c9c06a3e936a0b3c300
Devin Sweeting <devinsweeting@gmail.com>
2019-01-11 14:27:19 -0800

    create planetLifeExpectancy function

--- src/calculator.js
+++ src/calculator.js
@@ -21,30 +21,11 @@ class planetAge {
     return lifeExpect
   }
 
-  // calcMercury() {
-  //   var age = this.calcAge();
-  //   var ageMercury = (age / .24).toFixed(2);
-  //   return ageMercury;
-  // }
-  //
-  // calcVenus() {
-  //   var age = this.calcAge();
-  //   var ageVenus = (age / .62).toFixed(2);
-  //   return ageVenus;
-  // }
-  //
-  // calcMars() {
-  //   var age = this.calcAge();
-  //   var ageMars = (age / 1.88).toFixed(2);
-  //   return ageMars;
-  // }
-  //
-  // calcJupiter() {
-  //   var age = this.calcAge();
-  //   var ageJupiter = (age / 11.86).toFixed(2);
-  //   return ageJupiter;
-  // }
-
+  planetLifeExpectancy(planet) {
+    var lifeExpect = this.lifeExpectancy();
+    var planetLifeExpect = (lifeExpect / planet).toFixed(2);
+    return planetLifeExpect
+  }
 }
 
 export { planetAge };
--- src/main.js
+++ src/main.js
@@ -14,9 +14,8 @@ $(document).ready(function() {
     const age = birthday.calcAge();
     const ageOnPlanet = birthday.calcPlanetAge(planet);
     const lifeExpect = birthday.lifeExpectancy();
-    console.log("Expect to live for another " + lifeExpect + " years!");
-    console.log(planet);
-    console.log(age);
+    const planetLifeExpect = birthday.planetLifeExpectancy(planet);
     console.log("Your age on this planet is " + ageOnPlanet);
+    console.log("Expect to live for another " + planetLifeExpect + " years on this planet!");
   });
 });
