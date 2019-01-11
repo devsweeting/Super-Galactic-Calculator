f5f72132a462dd3db725177835f2f0a849c9c8bc
Devin Sweeting <devinsweeting@gmail.com>
2019-01-11 12:16:18 -0800

    write function and test for calcMercury method

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -3,16 +3,23 @@ import { planetAge } from './../src/calculator.js';
 describe('planetAge', function() {
 
 //   it('creates a user class of planetAge', function(){
-//     const userBirthday = ('March 14, 1995 03:24:00');
-//     const userBirthday = new Date;
-//     const user = new planetAge(userBirthday);
+//     var userBirthday = ('March 14, 1995 03:24:00');
+//     var userBirthday = new Date;
+//     var user = new planetAge(userBirthday);
 //     expect(user.birthday).toEqual(userBirthday);
 // });
 
   it('it will calculate a users age', function() {
-    const userBirthday = ('March 14, 1995 03:24:00')
-    const user = new planetAge(userBirthday);
+    var userBirthday = ('March 14, 1995 03:24:00')
+    var user = new planetAge(userBirthday);
     let age = user.calcAge()
     expect(age).toEqual(23)
+  });
+
+  it('it will calculate a users age on Mercury', function() {
+    var userBirthday = ('March 14, 1995 03:24:00')
+    var birthday = new planetAge(userBirthday);
+    let ageMercury = birthday.calcMercury()
+    expect(ageMercury).toEqual('95.83')
   })
 })
--- src/calculator.js
+++ src/calculator.js
@@ -1,11 +1,23 @@
 class planetAge {
   constructor(birthday) {
-    this.birthday = new Date(birthday)
+    this.birthday = new Date(birthday);
 }
 
   calcAge() {
-    var today = Date.now()
+    const today = Date.now();
     return ~~((today - this.birthday) / (31557600000));
+  }
+
+  calcMercury() {
+    var age = this.calcAge();
+    var ageMercury = (age / .24).toFixed(2);
+    return ageMercury;
+  }
+
+  calcMercury() {
+    var age = this.calcAge();
+    var ageMercury = (age / .62).toFixed(2);
+    return ageMercury;
   }
 }
 
